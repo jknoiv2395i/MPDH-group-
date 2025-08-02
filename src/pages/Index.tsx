36c898f9f49b28529ccdd75011765133f027f44d
jknoiv2395i <debramyabrato@gmail.com>
2025-08-02 22:47:43 +0530

    Revert "Add VisionSection component and update HeroContent styling"

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,10 @@
 import HeroSection from "@/components/HeroSection";
-import VisionSection from "@/components/VisionSection";
 import ServicesSection from "@/components/ServicesSection";
 
 const Index = () => {
   return (
     <>
       <HeroSection />
-      <VisionSection />
       <ServicesSection />
     </>
   );
